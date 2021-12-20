643f32dc320518b49a5d4ab6bc3f9a5de7baeadd
Akhila-setty27 <akhila.setty27@gmail.com>
2021-12-20 09:13:22 +0530

    Frame2 pushed

---
+++ frame2.js
@@ -0,0 +1,138 @@
+import React from 'react';
+
+
+
+
+
+var frame2 = React.createClass({
+    render: function() {
+      return (
+  
+        <nav>
+          <div className="logo">
+            <p>
+              <a href="#airway"><img src="/images/Vector.svg" alt="Airway" />
+              </a></p><a href="#airway">
+            </a></div><a href="#airway">
+          </a><ul><a href="#airway">
+            </a><div className="home"><a href="#airway">
+              </a><li><a href="#airway" /><a href="#Home">Home</a></li>
+            </div>
+            <div className="history">
+              <li><a href="#History">History</a></li>
+            </div>
+            <div className="points">
+              <li><a href="#Points">Points</a></li>
+            </div>
+            <div className="account">
+              <li><a href="#Account">Account</a></li>
+            </div>
+          </ul>
+          <div className="photo">
+            <img src="/images/Ellipse 1.svg" alt="Photo" />
+          </div>
+          <div className="photo2">
+            <img src="/images/Ellipse 2 (2).svg " alt="photo2" />
+          </div>
+          <div className="rectangle14">
+            <img src="images/Rectangle 14.svg" />
+          </div>
+          <div className="slide">
+            <a href><img src="/images/Rectangle 19.jpg" alt="slide" />
+            </a></div><a href>
+            <div className="earned">
+              <img src="/images/Rectangle 13.jpg" />
+              <div className="earned1">Total Points earned
+              </div>
+            </div>
+            <div className="rectangle">
+              <img src="/images/Rectangle 14.jpg" />
+            </div>  
+          </a><div className="pune"><a href>
+              <img src="/images/Rectangle 6.jpg" />
+            </a><div className="pune1"><a href>
+              </a><a href="#pune1">Trip to Pune</a>
+            </div>
+          </div>
+          <div className="chennai">
+            <img src="/images/Rectangle 6.jpg" />
+            <div className="chennai1">
+              <a href="#chennai1">Trip to Chennai</a>
+            </div>
+          </div>
+          <div className="bangalore">
+            <img src="/images/Rectangle 6.jpg" />
+            <div className="bangalore1">
+              <a href="#bangalore1">Trip to Bangalore</a>
+            </div>
+          </div>
+          <div className="leaf">
+            <img src="/images/leaves1.svg" alt="leaf" />
+          </div>
+          <div className="seventy">
+            <p>+70</p>
+          </div>
+          <div className="rectangle8">
+            <img src="/images/Rectangle 8.svg" />
+          </div>
+          <div className="ten">
+            <p>+10</p>
+          </div>
+          <div className="rectangle9">
+            <img src="/images/Rectangle 9.svg" />
+          </div>
+          <div className="twenty">
+            <p>-20</p>
+          </div>
+          <div className="rectangle10">
+            <img src="/images/Rectangle 10.svg" />
+          </div>
+          <div className="points_history">
+            <a href="#">Points History</a>
+          </div>
+          <div className="arrow1">
+            <a href="#"><img src="/images/Arrow left 1.jpg" alt="arrow1" /> 
+            </a></div><a href="#">
+          </a><div className="arrow2"><a href="#">
+            </a><a href="#"><img src="/images/Arrow left 1.jpg" alt="arrow2" /> 
+            </a></div><a href="#">
+          </a><div className="arrow3"><a href="#">
+            </a><a href="#"><img src="/images/Arrow left 1.jpg" alt="arrow3" /> 
+            </a></div><a href="#">
+            <div className="rectangle7">
+              <img src="images/Rectangle 7.svg" />
+            </div>
+            <div className="rectangle71">
+              <img src="images/Rectangle 7.svg" />
+            </div>
+            <div className="rectangle72">
+              <img src="images/Rectangle 7.svg" />
+            </div>
+          </a><div className="why"><a href="#">
+            </a><a href>Have negative points and don't know why?</a> 
+          </div>  
+          <div className="learn">
+            <a href>Learn more</a>
+          </div>
+          <div className="photo1">
+            <li><img src="/images/6543 1.jpg" alt="Photo1" /></li>
+          </div>
+          <div className="number">
+            <li><img src="/images/130.jpg" alt="number" /></li>
+          </div>
+          <div className="policy">
+            <a href="#policy">Privacy Policy</a>
+          </div>
+          <div className="terms">
+            <a href="#terms">Terms &amp; Conditions</a>
+          </div>
+          <div className="dot">
+            <img scr="/images/Ellipse4.jpg" />
+          </div>
+          <div className="air">
+            <a href="#air">©Airway 2020</a>
+          </div>
+        </nav>
+      );
+    }
+  });
