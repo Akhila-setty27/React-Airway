f313d72d021b67436598f8447e5280363198dbc6
AvanthiAnand <avanthianand2410@gmail.com>
2022-01-08 20:57:13 +0530

    frame8 pushed

---
+++ frame8.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import 'frame8.css' ;
+var NewComponent = React.createClass({
+  render: function() {
+    return (
+
+      <nav>
+        <div className="logo">
+          <p>
+            <a href="#airway"><img src="/images/Vector.svg" alt="Airway" />
+            </a></p><a href="#airway">
+          </a></div><a href="#airway">
+        </a><div className="home"><a href="#airway">
+          </a><a href="#Home">Home</a>
+        </div>
+        <div className="history">
+          <a href="#History">History</a>
+        </div>
+        <div className="points">
+          <a href="#Points">Points</a>
+        </div>
+        <div className="account">
+          <a href="#Account">Account</a>
+        </div>
+        <div className="photo">
+          <a href="#"><img src="/images/Ellipse 1.svg" alt="Photo" />
+          </a></div><a href="#"> 
+        </a><div className="photo2"><a href="#">
+          </a><a href="#"><img src="/images/Ellipse 2.svg " alt="photo2" />
+          </a></div><a href="#">
+        </a><form action="#"><a href="#">
+          </a><div className="con1"><a href="#">            
+              <header className="head-form">
+                <h3>Hey Adi, Where to next?</h3>            
+              </header>
+              <br />
+              <div className="field-set">
+                <header className="head-form">
+                  <h3>Hey Adi, Where to next?</h3>            
+                </header>
+                <div className="line">
+                  <img src="images/Line 4.svg" />
+                </div>
+                <div className="input_box">
+                  <label htmlFor="source">Source</label><br />
+                  <input className="source" id="source" placeholder="Eg. Bangalore" type="text" required /><br />
+                  {/*<div class="icon1"><img src="images/User.svg"/></div>*/}
+                </div>
+                <div className="input_box"> 
+                  <label htmlFor="destination">Destination</label><br />
+                  <input className="destination" id="destination" placeholder="Eg. Pune" type="text" required /><br />
+                  {/*<div class="icon2"><img src="images/Message open.svg"/></div>*/}
+                </div>
+                <div className="input_box">
+                  <label htmlFor="date">Date</label><br />
+                  <input className="date" id="date" placeholder="30-03-2021" type="text" required /><br />
+                  <div className="calendar"><img src="images/calendar.jpg" /></div>
+                </div>               
+                <br />
+              </div>
+            </a><button className="find_tickets"><a href="#">
+              </a><a href="#find_tickets"> Find tickets </a></button>
+            <div className="con2"> 
+              <header className="head-form">
+                <h2>Search Results:</h2>            
+              </header>
+              <div className="con21">
+                <p className="bangalore1">Bangalore</p>
+                <p className="chennai1">Chennai</p>
+                <p className="march1">30 March</p>
+                <p className="hr1">08:00 hr estimated time</p>
+                <p className="jet1">Jet Airways</p>
+                <div className="ellipse1371"><img src="images/Ellipse 137.svg" /></div>
+                <div className="line11"><img src="images/Line 1.svg" /></div>
+                <div className="xs11"><img src="images/Ellipse xs.svg" /></div>
+                <div className="xs21"><img src="images/Ellipse xs.svg" /></div>
+                <div className="xs31"><img src="images/Ellipse xs.svg" /></div>
+                <div className="xs41"><img src="images/Ellipse xs.svg" /></div>
+                <div className="xs51"><img src="images/Ellipse xs.svg" /></div>
+                <div className="xs61"><img src="images/Ellipse xs.svg" /></div>
+                <div className="xs71"><img src="images/Ellipse xs.svg" /></div>
+                <div className="xs81"><img src="images/Ellipse xs.svg" /></div>
+                <div className="s11"><img src="images/Ellipse s.svg" /></div>
+                <div className="s21"><img src="images/Ellipse s.svg" /></div>
+                <div className="arr1"><img src="images/Vector arrow.svg" /></div>
+                <div className="arrbox1">
+                  <a href="#"><img src="/images/Arrow left 1.svg" alt="Arrow" />
+                  </a></div><a href="#">
+                </a></div><a href="#">
+              </a><div className="con22"><a href="#">
+                  <p className="bangalore2">Bangalore</p>
+                  <p className="chennai2">Chennai</p>
+                  <p className="march2">30 March</p>
+                  <p className="hr2">09:30 hr estimated time</p>
+                  <p className="jet2">Trial Airways</p>
+                  <div className="ellipse1372"><img src="images/Ellipse 137.svg" /></div>
+                  <div className="line12"><img src="images/Line 1.svg" /></div>
+                  <div className="xs12"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs22"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs32"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs42"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs52"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs62"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs72"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs82"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="s12"><img src="images/Ellipse s.svg" /></div>
+                  <div className="s22"><img src="images/Ellipse s.svg" /></div>
+                  <div className="arr2"><img src="images/Vector arrow.svg" /></div>
+                </a><div className="arrbox2"><a href="#">
+                  </a><a href="#"><img src="/images/Arrow left 1.svg" alt="Arrow" />
+                  </a></div><a href="#">
+                </a></div><a href="#">
+              </a><div className="con23"><a href="#">
+                  <p className="bangalore3">Bangalore</p>
+                  <p className="chennai3">Chennai</p>
+                  <p className="march3">30 March</p>
+                  <p className="hr3">09:30 hr estimated time</p>
+                  <p className="jet3">Spice Airways</p>
+                  <div className="ellipse1373"><img src="images/Ellipse 137.svg" /></div>
+                  <div className="line13"><img src="images/Line 1.svg" /></div>
+                  <div className="xs13"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs23"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs33"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs43"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs53"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs63"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs73"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="xs83"><img src="images/Ellipse xs.svg" /></div>
+                  <div className="s13"><img src="images/Ellipse s.svg" /></div>
+                  <div className="s23"><img src="images/Ellipse s.svg" /></div>
+                  <div className="arr3"><img src="images/Vector arrow.svg" /></div>
+                </a><div className="arrbox3"><a href="#">
+                  </a><a href="#"><img src="/images/Arrow left 1.svg" alt="Arrow" />
+                  </a></div><a href="#">
+                </a><div className="seemore"><a href="#">
+                  </a><a href="#">See_more</a>
+                </div>
+                <div className="seemorearr">
+                  <a href="#">&gt;</a>
+                </div>
+              </div>
+              <br />
+            </div></div></form></nav>
+    );
+  }
+});
